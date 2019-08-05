d6e76c93c133eb28aec1ea4ea28f081c6c6c5fe4
Tony Zhang <z_haijun@foxmail.com>
2019-08-05 08:57:06 +0800

    Use promiseall for sync

--- spider.js
+++ spider.js
@@ -1,101 +1,48 @@
-var month='2019-07';
-var date;
-var day=1;
-var fs = require('fs'); 
-var iconv = require('iconv-lite');
 
-//fetchDealInfoByMonth2();
-for(let day=1; day < 31; day++){      
+const fs = require('fs'); 
+const fetch = require("node-fetch");
+const HttpProxyAgent = require('http-proxy-agent');
+var plist = [];
+var month='2019-05';
+var date;
+
+for(let day=1; day < 32; day++){      
     date = month + '-' + (day>9? day: ('0' + day));  
-    fetchDealInfoByDate2(date);
+
+    var url = 'http://www.czce.com.cn/cn/DFSStaticFiles/Future/2019/'
+        + date.replace(/-/g,'')+'/FutureDataDelsettle.txt';
+
+    plist.push(fetch(url,{agent: new HttpProxyAgent("http://127.0.0.1:8888")}).then(response=>{return response.text()}));
 };
 
-function fetchDealInfoByMonth2()
-{    
-    day = day +1;
-    date = month + '-' + (day>9? day: ('0' + day));  
+var filename = './'+ month +'.txt';
+var fileContent = '日期      |合约代码|卖方会员  |会员简称|买方会员  |会员简称|交割量（手）\r\n\r\n';
+Promise.all(plist).then(result =>{
 
-    if(day < 31){
-        fetchDealInfoByDateSync(date).then(function(data){
-            fetchDealInfoByMonth2();
-        });
-    }
-}
+    for(let pi=0; pi<result.length; pi++){
+        var contractCode;
+        let date;
+        let lines = result[pi].split('\r\n');
+        for (let i = 0; i < lines.length; i++) {
 
-function fetchDealInfoByDate(date) {
+            if (lines[i].match(/[A-Z0-9]{5}/) != null) {
+                contractCode = lines[i].match(/[A-Z0-9]{5}/)[0];
+            }
+            if (lines[i].match(/\d{4}-\d{2}-\d{2}/) != null) {
+                date = lines[i].match(/\d{4}-\d{2}-\d{2}/)[0];
+            }
+            if (lines[i].match(/期货\|/) != null) {
 
-    var request = require("request");
-    var url = 'http://www.czce.com.cn/cn/DFSStaticFiles/Future/2019/'
-            + date.replace(/-/g,'')+'/FutureDataDelsettle.txt';
-      
-    request(url, function (error, response, body) { 
-        if (!error && response.statusCode == 200) {
-            var contractCode; 
-            var lines = body.split('\r\n');
-            for (let i=0;i<lines.length; i++){
-    
-                if(lines[i].match(/[A-Z0-9]{5}/) != null){
-                    contractCode = lines[i].match(/[A-Z0-9]{5}/)[0];
-                }
-                if(lines[i].match(/期货\|/) != null){
-                    console.log(date + "|" + contractCode + '|'+ lines[i]+ '|'+ new Date().getSeconds());
-                }
+                lines[i] = date + "|" + contractCode + '   |' + lines[i];
+                fileContent= fileContent + lines[i]+'\r\n';
             }
         }
-    });    
-}
+    }
 
-function fetchDealInfoByDate2(date) {
+    fs.writeFile(filename,fileContent,err=>{if(err) throw err;});
 
-    var fetch = require("node-fetch");
-    var url = 'http://www.czce.com.cn/cn/DFSStaticFiles/Future/2019/'
-            + date.replace(/-/g,'')+'/FutureDataDelsettle.txt';
-      
-    fetch(url).then(response=>{
-        response.text().then(body=>{
-            var contractCode; 
-            var lines = body.split('\r\n');
-            for (let i=0;i<lines.length; i++){
-    
-                if(lines[i].match(/[A-Z0-9]{5}/) != null){
-                    contractCode = lines[i].match(/[A-Z0-9]{5}/)[0];
-                }
-                if(lines[i].match(/期货\|/) != null){
+}).catch(err=>console.log(err.message));
 
-                    lines[i] = date + "|" + contractCode + '|'+ lines[i] + '\r\n';
-                    console.log(lines[i] );
-                    fs.appendFile('./try4.txt',lines[i],err=>{if(err) throw err;});
-                }
-            }
-        })
-    });  
-}
 
-function fetchDealInfoByDateSync(date) {
-
-    var request = require("request");
-    var url = 'http://www.czce.com.cn/cn/DFSStaticFiles/Future/2019/'
-            + date.replace(/-/g,'')+'/FutureDataDelsettle.txt';
-         
-    return new Promise(function (resolve, reject) {
-
-        request(url, function (error, response, body) { 
-            if (!error && response.statusCode == 200) {
-                var contractCode; 
-                var lines = body.split('\r\n');
-                for (let i=0;i<lines.length; i++){
-        
-                    if(lines[i].match(/[A-Z0-9]{5}/) != null){
-                        contractCode = lines[i].match(/[A-Z0-9]{5}/)[0];
-                    }
-                    if(lines[i].match(/期货\|/) != null){
-                        console.log(date + "|" + contractCode + '|'+ lines[i]+ '|'+ new Date().getSeconds());
-                    }
-                }
-            }
-            resolve(body);
-        });  
-    });
-}
 
 
